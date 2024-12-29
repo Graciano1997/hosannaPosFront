c57b7489240ed02fad0779386a49ab3a0b9be7e7
Graciano1997 <gracianomanuelhenrique@gmail.com>
2024-12-29 16:18:04 +0100

    added the react router

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { BackwardIcon } from '@heroicons/react/16/solid'
+import './App.css'
+
+function App() {
+  return (
+    <>
+     <div className="h-screen w-screen bg-gray-100">
+     </div>
+    </>
+  )
+}
+
+export default App
