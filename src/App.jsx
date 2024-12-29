31f82febb211ee67baee9e9b7944ea0ba75e3368
Graciano1997 <gracianomanuelhenrique@gmail.com>
2024-12-30 00:02:37 +0100

    add the multilanguage support

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import { BackwardIcon } from '@heroicons/react/16/solid'
 import './App.css'
+import Dashboard from './components/Dashboard'
+import Header from './components/Header'
 
 function App() {
   return (
     <>
-     <div className="h-screen w-screen bg-gray-100">
+     <div className="h-screen w-screen p-3">
+      <Header/>
+      <Dashboard/>
      </div>
     </>
   )
