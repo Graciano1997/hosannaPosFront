0e551d45f2fd5bac970482f5f263fdfe6b648788
Graciano1997 <gracianomanuelhenrique@gmail.com>
2025-01-04 07:04:07 +0100

    add the request KSX

---
+++ src/components/Request.jsx
@@ -0,0 +1,21 @@
+import { useTranslation } from "react-i18next";
+import Card from "./Card";
+import Title from "./Title";
+
+const Request=()=>{
+    const {t}=useTranslation();
+    return(
+        <>
+        <Title title={'Request'}/>
+        <div className="flex flex-wrap justify-center gap-7 mt-4 p-1 ">
+        <Card className="font-black" width={400} height={350}  info={{title:t('month_balance'),description:t('about')}}/>
+        <Card width={400} height={350}  info={{title:t('month_balance'),description:t('about')}}/>
+        <Card width={400} height={350} info={{title:t('medicines_status'),description:t('about')}}/>
+        <Card width={600} height={350} info={{title:t('month_balance'),description:t('about')}}/>
+        <Card width={600} height={350} info={{title:t('month_balance'),description:t('about')}}/>
+        </div>
+        </>
+    )
+};
+
+export default Request;
