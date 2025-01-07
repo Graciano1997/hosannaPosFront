97f2c6bfb1d7563b998d6ac72b534f30f9d2c2c0
Graciano1997 <gracianomanuelhenrique@gmail.com>
2025-01-07 23:59:57 +0100

    fix the chart

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,7 +1,7 @@
 import { useTranslation } from "react-i18next";
 import Card from "./Card";
 import Title from "./Title";
-import BarChart from "./BarChart";
+import { BarChart } from "./BarChart";
 
 const Dashboard=()=>{
     const {t}=useTranslation();
@@ -10,7 +10,7 @@ const Dashboard=()=>{
         {/* <Title title={'Dashboard'}/> */}
         <div className="flex flex-wrap justify-center gap-7 mt-4 p-1 ">
         <Card className="font-black" width={400} height={350}  info={{title:t('month_balance'),description:t('about')}}/>
-        <BarChart/>
+        <BarChart />
         <Card width={400} height={350} info={{title:t('medicines_status'),description:t('about')}}/>
         <Card width={600} height={350} info={{title:t('month_balance'),description:t('about')}}/>
         <Card width={600} height={350} info={{title:t('month_balance'),description:t('about')}}/>
