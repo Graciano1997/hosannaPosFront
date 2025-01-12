ef74f2f6f9d351944385976bb0fbe26ba7967534
Graciano1997 <gracianomanuelhenrique@gmail.com>
2025-01-12 23:38:12 +0100

    added the expired component

--- src/components/LastExpired.jsx
+++ src/components/LastExpired.jsx
@@ -1,27 +1,29 @@
 import { useTranslation } from "react-i18next";
 import CardTitle from "./CardTitle";
 
-const LastSelling =({width=200,height=300,info})=>{    
+const LastExpired =({width=200,height=300,info})=>{    
 
     const {t}=useTranslation();
     
-    const lastSellings= [{name:"Amoxi",qty:5,total:1000},{name:"Neurobion",qty:5,total:1000},
-        {name:"Vitamina C",qty:5,total:1000}];
+    const expireds= [{name:"Amoxi",qty:5,total:1000,data:{expire:'12-12-2024',issue:'11-11-2020'}},{name:"Neurobion",qty:5,total:1000,data:{expire:'12-12-2024',issue:'11-11-2020'}},
+        {name:"Vitamina C",qty:5,total:1000,data:{expire:'12-12-2024',issue:'11-11-2020'}}];
 
     return(
          <div style={{height:height,width:width}} className={`grid grid-rows-[50px_auto] bg-white rounded shadow-md`}>
              <CardTitle title={info.title} />
              <div className="p-2">
                 <ul className="h-[100%] flex flex-col justify-around">
-                <li className="h-[40px] bg-white  grid grid-cols-3 items-center justify-center">
+                <li className="h-[40px] bg-white  grid grid-cols-4 items-center justify-center">
                     <p>{'Nome'}</p>
                     <p>{'Qty'}</p>
                     <p>{'Total'}</p>
+                    <p>{'Exp.Date'}</p>
                     </li>
-                    {lastSellings.map((el)=><li className="h-[40px] bg-green-100 cursor-pointer justify-center p-1 rounded sm:shadow grid grid-cols-3 items-center">
+                    {expireds.map((el)=><li className="h-[40px] bg-red-100 cursor-pointer justify-center p-1 rounded sm:shadow grid grid-cols-3 items-center">
                     <p>{el.name}</p>
                     <p>{el.qty}</p>
                     <p>{el.total} {'kz'}</p>
+                    <p>{el.data.expire} {'kz'}</p>
                     </li>)}
                 </ul>
              </div>
@@ -29,4 +31,4 @@ const LastSelling =({width=200,height=300,info})=>{
     );
 };
 
-export default LastSelling;
+export default LastExpired;
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,7 +2,7 @@ import { EllipsisHorizontalIcon } from "@heroicons/react/24/solid";
 
 const Title=({title})=>{
     return(
-        <div className="flex justify-between items-center w-[100%] h-[100px] bg-white rounded p-4">
+        <div className="mt-[2rem] flex justify-between items-center w-[100%] h-[100px] bg-white rounded p-4">
             <h1 className="text-3xl">{title}</h1>
             <EllipsisHorizontalIcon className="w-7 y-7 text-[#323232] rounded cursor-pointer hover:shadow-sm"/>
         </div>
