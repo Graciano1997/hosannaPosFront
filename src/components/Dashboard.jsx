d0abd2be8363d90f018853cf521bdfb5449dd785
Graciano1997 <gracianomanuelhenrique@gmail.com>
2025-01-12 23:38:12 +0100

    added the expired component

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -2,11 +2,13 @@ import { useTranslation } from "react-i18next";
 import Card from "./Card";
 import { BarChart } from "./BarChart";
 import LastSelling from "./LastSelling";
+import Title from "./Title";
 
 const Dashboard=()=>{
     const {t}=useTranslation();
     return(
         <>
+        <Title title={t('dashboard')}/>
         <div className="flex flex-wrap justify-center gap-7 mt-4 p-1 ">
         <Card className="font-black" width={400} height={350}  info={{title:t('month_balance'),description:t('about')}}/>
         <BarChart width={450} height={350} info={'Gráfico Semestral'} />
