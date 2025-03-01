408c3f30221cfd770c57e4207c1a8807627cf6e5
Graciano1997 <gracianomanuelhenrique@gmail.com>
2025-03-02 00:15:40 +0100

    updat the dashboard

--- src/components/general/CardTitle.jsx
+++ src/components/general/CardTitle.jsx
@@ -1,6 +1,6 @@
 const CardTitle=({title})=>{
     return(
-        <div className="shadow bg-white-500 pl-4 flex justify-begin items-center rounded-t">
+        <div className="shadow bg-white-500 pl-4 flex justify-center items-center rounded-t">
         <h2 className="text-center">{title}</h2>
      </div>
     );
