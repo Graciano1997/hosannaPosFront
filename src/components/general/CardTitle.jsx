a77c827ad7b9727d528cfe44b626721fb966041f
Graciano1997 <gracianomanuelhenrique@gmail.com>
2025-03-02 23:30:54 +0100

    added the anual graph

--- src/components/general/CardTitle.jsx
+++ src/components/general/CardTitle.jsx
@@ -1,7 +1,9 @@
+import { firstCapitalize } from "../../lib/firstCapitalize";
+
 const CardTitle=({title})=>{
     return(
         <div className="shadow bg-white-500 pl-4 flex justify-center items-center rounded-t">
-        <h2 className="text-center">{title}</h2>
+        <h2 className="text-center">{firstCapitalize(title)}</h2>
      </div>
     );
 };
