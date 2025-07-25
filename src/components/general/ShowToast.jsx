779e830608e9ecb2028bddf618c769981aee579f
Graciano1997 <gracianomanuelhenrique@gmail.com>
2025-07-25 05:50:31 +0100

    adjusting the show toast UI

--- src/components/general/ShowToast.jsx
+++ src/components/general/ShowToast.jsx
@@ -10,15 +10,15 @@ const ShowToast = ({object})=>{
   },6000);
 
     return(
-       <div className={`bg-black opacity-90 w-[400px]
+       <div className={`bg-black opacity-90 w-100 w-100  md:w-[400px]
         z-[1000]
         ${object.success?'text-green-500':''}
         ${object.error?'text-red-500':''}
         ${object.warning?'text-yellow-500':''}
         flex justify-center items-center
-        p-4 fixed bottom-[10px] right-[10px]
+        p-5 fixed bottom-[10px] right-[10px]
         rounded-[16px] flex-wrap`}>
-        <XMarkIcon onClick={()=>{dispatch(closeToast())}} className={`w-8 h-8 ${object.success?'text-green-300':''} ${object.error?'text-white':''} ${object.warning?'text-yellow-300':''}  absolute right-0 top-[5px] cursor-pointer `}/>
+        <XMarkIcon onClick={()=>{dispatch(closeToast())}} className={`w-6 h-6 ${object.success?'text-green-300':''} ${object.error?'text-white':''} ${object.warning?'text-yellow-300':''}  absolute right-2 top-[5px] cursor-pointer `}/>
         <p>{object.message}</p>
       </div>
     );
