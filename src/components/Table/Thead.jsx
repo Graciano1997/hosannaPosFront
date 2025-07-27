80020f07534d48d53b06d0b94b9a6eaca94f8f74
Graciano1997 <gracianomanuelhenrique@gmail.com>
2025-07-27 09:20:33 +0100

    fix the sortCollection issue

--- src/components/Table/Thead.jsx
+++ src/components/Table/Thead.jsx
@@ -1,15 +1,36 @@
 import { useTranslation } from "react-i18next";
 import { firstCapitalize } from "../../lib/firstCapitalize";
+import { ChevronDownIcon, ChevronUpIcon} from "@heroicons/react/24/solid";
+import { SortIcon } from "../general/SortIcon";
+import { useState } from "react";
+import { sortCollection } from "../../lib/sortCollection";
 
-const Thead = ({ object, filterRows }) => {
+const Thead = ({ object, filterRows, items=[] }) => {
 
   const { t } = useTranslation();
+  const [activeSortCol,setActiveSortCol]=useState('id');
+  const [activeSortOrdDesc,setActiveSortOrdDesc]=useState(false);
+  const sortingFieldFilter=['image'];
 
   const keys = Object.keys(object).filter((item) => !filterRows.includes(item));
   return (
     <thead className="sticky bg-white top-[-8px]" >
       <tr className="p-2 shadow h-[45px]">
-        {keys.map((label) => <th className="p-2 text-sm">{firstCapitalize(t(label))}</th>)}
+        {keys.map((label) => 
+        <th className="p-2 text-sm cursor-pointer" onClick={()=>{
+          if(activeSortCol==label){
+            setActiveSortOrdDesc(!activeSortOrdDesc);
+          }else{
+            setActiveSortOrdDesc(false);
+            if(!sortingFieldFilter.includes(label))
+              setActiveSortCol(label);
+          }
+          console.log(sortCollection(items,label,activeSortOrdDesc));
+          }}>
+            <div className="flex justify-center items-center gap-3 ">{firstCapitalize(t(label))}
+              {<SortIcon desc={activeSortOrdDesc} show={ sortingFieldFilter.includes(label)? false: label==activeSortCol}/>}
+            </div>
+          </th>)}
         <th className="p-2">{' '}</th>
       </tr>
     </thead>
--- src/lib/sortCollection.js
+++ src/lib/sortCollection.js
@@ -1,21 +1,27 @@
-export const sortCollection = (collection,key,desc=false,date=false)=>{
-    for(let i=0; i< collection.length; i++){
-        for(let j=0; j<collection.length - 1;j++){
+export const sortCollection = (collectionArr,key,desc=false,date=false)=>{
 
-            if(date){
-                if( (new Date(collection[j][key])) > (new Date(collection[j+1][key]))){
-                    const auxiliar = collection[j][key];
-                    collection[j][key]=collection[j+1][key];
-                    collection[j+1][key]=auxiliar;
-                }
-            }else{
+    const collection = [...collectionArr]
+    for(let i=0; i < collection.length; i++){
+        for(let j=0; j<collection.length-1; j++){
+
+             if(date){
+                 if( (new Date(collection[j][key])) > (new Date(collection[j+1][key]))){
+                     let temp = collection[j][key];
+                     collection[j][key]=collection[j+1][key];
+                     collection[j+1][key]=temp;
+                 }
+             }else{
                 if(collection[j][key] > collection[j+1][key]){
-                    const auxiliar = collection[j][key];
-                    collection[j][key]=collection[j+1][key]
-                    collection[j+1][key]=auxiliar;
+                    let temp = collection[j];
+                    collection[j]=collection[j+1]
+                    collection[j+1]=temp;
                 }
             }
         }
     }
-        return collection;    
+    if(desc)
+        return collection.reverse();
+    
+    return collection;
 }
+
